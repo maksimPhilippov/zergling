62421647640404d16538660046967e1ef605d626
maksim <maksimphilippov@yahoo.com>
2025-02-27 01:51:11 +0400

    Sorting of variables and mark registration time

--- zergling.sh
+++ zergling.sh
@@ -4,13 +4,16 @@ START=99
 STOP=10
 USE_PROCD=1
 
-OVERLORD_USER=${OVERLORD_USER:-"overlord"}
+# Probably you want to change
 OVERLORD_HOST=${OVERLORD_HOST:-"example.com"}
 OVERLORD_PORT=${OVERLORD_PORT:-22}
-LOCAL_SSH_PORT=${LOCAL_SSH_PORT:-22}
-SSH_KEY=${SSH_KEY:-"/root/.ssh/zerling_ssh_key"}
 SSH_TUNNEL_PORT=${SSH_TUNNEL_PORT:-}
 ZERLING_ID=${ZERLING_ID:-1234}
+
+# Probably you don't want to change
+OVERLORD_USER=${OVERLORD_USER:-"overlord"}
+LOCAL_SSH_PORT=${LOCAL_SSH_PORT:-22}
+SSH_KEY=${SSH_KEY:-"/root/.ssh/zerling_ssh_key"}
 REG_FILE=${REG_FILE:-"/var/lib/regs/${ZERLING_ID}"}
 REG_VAR="port is ${SSH_TUNNEL_PORT}"
 
@@ -38,7 +41,7 @@ start_service() {
     procd_close_instance
     
     # Create the registration file on the remote server
-    ssh -i ${SSH_KEY} ${OVERLORD_USER}@${OVERLORD_HOST} "mkdir -p $(dirname ${REG_FILE}) && echo ${REG_VAR} > ${REG_FILE}"
+    ssh -i ${SSH_KEY} ${OVERLORD_USER}@${OVERLORD_HOST} "mkdir -p $(dirname ${REG_FILE}) && echo ${REG_VAR} > ${REG_FILE} && date >> ${REG_FILE}"
     logger -t zerling "SSH tunnel established and remote registration file created."
 }
 
