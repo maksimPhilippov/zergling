afc557462acc1aaeef6d03ffa52b537278dba2b1
maksim <maksimphilippov@yahoo.com>
2025-03-01 14:26:47 +0400

    Fix g mistype in zerling word

--- zergling.sh
+++ zergling.sh
@@ -8,24 +8,24 @@ USE_PROCD=1
 OVERLORD_HOST=${OVERLORD_HOST:-"example.com"}
 OVERLORD_PORT=${OVERLORD_PORT:-22}
 SSH_TUNNEL_PORT=${SSH_TUNNEL_PORT:-2000}
-ZERLING_ID=${ZERLING_ID:-"ZerlingID"}
+ZERGLING_ID=${ZERGLING_ID:-"ZerglingID"}
 
 # Probably you don't want to change
 OVERLORD_USER=${OVERLORD_USER:-"overlord"}
 LOCAL_SSH_PORT=${LOCAL_SSH_PORT:-22}
-SSH_KEY=${SSH_KEY:-"/root/.ssh/zerling_ssh_key"}
-REG_FILE=${REG_FILE:-"/var/lib/regs/${ZERLING_ID}"}
+SSH_KEY=${SSH_KEY:-"/root/.ssh/zergling_ssh_key"}
+REG_FILE=${REG_FILE:-"/var/lib/regs/${ZERGLING_ID}"}
 REG_VAR="port is ${SSH_TUNNEL_PORT}"
 ETHERNAL_LOOP="while true ; do sleep 1h ; done"
 
 start_service() {
-    logger -t zerling "Starting SSH tunnel service..."
+    logger -t zergling "Starting SSH tunnel service..."
     
     # Wait until the internet connection is available
     while ! ping -c 1 1.1.1.1 >/dev/null 2>&1; do
         sleep 5
     done
-    logger -t zerling "Internet connection detected."
+    logger -t zergling "Internet connection detected."
     
     # Start reverse SSH tunnel using autossh
     procd_open_instance
@@ -41,16 +41,16 @@ start_service() {
     
     # Create the registration file on the remote server
     ssh -i ${SSH_KEY} ${OVERLORD_USER}@${OVERLORD_HOST} "mkdir -p $(dirname ${REG_FILE}) && echo ${REG_VAR} > ${REG_FILE} && date >> ${REG_FILE}"
-    logger -t zerling "SSH tunnel established and remote registration file created."
+    logger -t zergling "SSH tunnel established and remote registration file created."
 }
 
 stop_service() {
-    logger -t zerling "Stopping SSH tunnel service..."
+    logger -t zergling "Stopping SSH tunnel service..."
     procd_kill
     
     # Remove the registration file on the remote server
     ssh -i ${SSH_KEY} ${OVERLORD_USER}@${OVERLORD_HOST} "rm -f ${REG_FILE}"
-    logger -t zerling "Remote registration file removed."
+    logger -t zergling "Remote registration file removed."
     
-    logger -t zerling "SSH tunnel stopped."
+    logger -t zergling "SSH tunnel stopped."
 }
