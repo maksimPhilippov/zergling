005ca6d39f51e9eed624aa9a7ea3ff38dae45db6
maksim <maksimphilippov@yahoo.com>
2025-03-01 14:13:19 +0400

    Fix ssh connection
    * Fixed connection by executing infinite loop on other side
    * Better procd respawn and logging support
    * removed dependency on autossh

--- zergling.sh
+++ zergling.sh
@@ -7,8 +7,8 @@ USE_PROCD=1
 # Probably you want to change
 OVERLORD_HOST=${OVERLORD_HOST:-"example.com"}
 OVERLORD_PORT=${OVERLORD_PORT:-22}
-SSH_TUNNEL_PORT=${SSH_TUNNEL_PORT:-}
-ZERLING_ID=${ZERLING_ID:-1234}
+SSH_TUNNEL_PORT=${SSH_TUNNEL_PORT:-2000}
+ZERLING_ID=${ZERLING_ID:-"ZerlingID"}
 
 # Probably you don't want to change
 OVERLORD_USER=${OVERLORD_USER:-"overlord"}
@@ -16,15 +16,10 @@ LOCAL_SSH_PORT=${LOCAL_SSH_PORT:-22}
 SSH_KEY=${SSH_KEY:-"/root/.ssh/zerling_ssh_key"}
 REG_FILE=${REG_FILE:-"/var/lib/regs/${ZERLING_ID}"}
 REG_VAR="port is ${SSH_TUNNEL_PORT}"
+ETHERNAL_LOOP="while true ; do sleep 1h ; done"
 
 start_service() {
     logger -t zerling "Starting SSH tunnel service..."
-    
-    # Check if autossh is installed
-    if ! command -v autossh >/dev/null 2>&1; then
-        logger -t zerling "Error: autossh is not installed. Exiting."
-        exit 1
-    fi
     
     # Wait until the internet connection is available
     while ! ping -c 1 1.1.1.1 >/dev/null 2>&1; do
@@ -34,10 +29,14 @@ start_service() {
     
     # Start reverse SSH tunnel using autossh
     procd_open_instance
-    procd_set_param command /usr/sbin/autossh -M 0 -N -R 127.0.0.1:${SSH_TUNNEL_PORT}:localhost:${LOCAL_SSH_PORT} \
+    procd_set_param command /usr/bin/ssh -N -R 127.0.0.1:${SSH_TUNNEL_PORT}:localhost:${LOCAL_SSH_PORT} \
         -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes \
-        -i ${SSH_KEY} ${OVERLORD_USER}@${OVERLORD_HOST} -p ${OVERLORD_PORT}
-    procd_set_param respawn
+        -i ${SSH_KEY} ${OVERLORD_USER}@${OVERLORD_HOST} -p ${OVERLORD_PORT} \
+        "$ETHERNAL_LOOP"
+    procd_set_param env HOME=/root
+    procd_set_param stdout 1 # forward stdout of the command to logd
+    procd_set_param stderr 1 # same for stderr
+    procd_set_param respawn ${respawn_threshold:-3600} ${respawn_timeout:-5} ${respawn_retry:-5}
     procd_close_instance
     
     # Create the registration file on the remote server
