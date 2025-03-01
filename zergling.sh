4bfd39b1d01b5b1415edfca50a44c26ab065a2f4
maksim <maksimphilippov@yahoo.com>
2025-03-01 13:21:25 +0400

    Refactor to usual sh

--- zergling.sh
+++ zergling.sh
@@ -1,8 +1,7 @@
-#!/bin/sh /etc/rc.common
+#!/bin/sh 
 
 START=99
 STOP=10
-USE_PROCD=1
 
 # Probably you want to change
 OVERLORD_HOST=${OVERLORD_HOST:-"example.com"}
@@ -17,14 +16,10 @@ SSH_KEY=${SSH_KEY:-"/root/.ssh/zerling_ssh_key"}
 REG_FILE=${REG_FILE:-"/var/lib/regs/${ZERLING_ID}"}
 REG_VAR="port is ${SSH_TUNNEL_PORT}"
 
-start_service() {
+export HOME=/root
+
+start() {
     logger -t zerling "Starting SSH tunnel service..."
-    
-    # Check if autossh is installed
-    if ! command -v autossh >/dev/null 2>&1; then
-        logger -t zerling "Error: autossh is not installed. Exiting."
-        exit 1
-    fi
     
     # Wait until the internet connection is available
     while ! ping -c 1 1.1.1.1 >/dev/null 2>&1; do
@@ -33,25 +28,33 @@ start_service() {
     logger -t zerling "Internet connection detected."
     
     # Start reverse SSH tunnel using autossh
-    procd_open_instance
-    procd_set_param command /usr/sbin/autossh -M 0 -N -R 127.0.0.1:${SSH_TUNNEL_PORT}:localhost:${LOCAL_SSH_PORT} \
+    
+    /usr/bin/ssh -T -f -R 127.0.0.1:${SSH_TUNNEL_PORT}:localhost:${LOCAL_SSH_PORT} \
         -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes \
         -i ${SSH_KEY} ${OVERLORD_USER}@${OVERLORD_HOST} -p ${OVERLORD_PORT}
-    procd_set_param respawn
-    procd_close_instance
     
     # Create the registration file on the remote server
-    ssh -i ${SSH_KEY} ${OVERLORD_USER}@${OVERLORD_HOST} "mkdir -p $(dirname ${REG_FILE}) && echo ${REG_VAR} > ${REG_FILE} && date >> ${REG_FILE}"
-    logger -t zerling "SSH tunnel established and remote registration file created."
+    #ssh -i ${SSH_KEY} ${OVERLORD_USER}@${OVERLORD_HOST} "mkdir -p $(dirname ${REG_FILE}) && echo ${REG_VAR} > ${REG_FILE} && date >> ${REG_FILE}"
+    #logger -t zerling "SSH tunnel established and remote registration file created."
 }
 
-stop_service() {
+stop() {
     logger -t zerling "Stopping SSH tunnel service..."
-    procd_kill
     
+    TUNNEL_PID=`ps | grep -v grep | grep ${OVERLORD_HOST}`
     # Remove the registration file on the remote server
-    ssh -i ${SSH_KEY} ${OVERLORD_USER}@${OVERLORD_HOST} "rm -f ${REG_FILE}"
-    logger -t zerling "Remote registration file removed."
+    #ssh -i ${SSH_KEY} ${OVERLORD_USER}@${OVERLORD_HOST} "rm -f ${REG_FILE}"
+    #logger -t zerling "Remote registration file removed."
+    kill "$TUNNEL_PID"
     
-    logger -t zerling "SSH tunnel stopped."
+    logger -t zerling "SSH tunnel $TUNNEL_PID stopped."
 }
+
+case "$1" in
+start) start ;;
+stop) stop ;;
+help) echo "usage: zerling {start|stop}"
+esac
+
+exit 0
+
